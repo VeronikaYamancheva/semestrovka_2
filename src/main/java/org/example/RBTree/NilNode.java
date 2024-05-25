cbdcb0101aa4440a23e326375910c5389da3164e
Veronika <vshrony@email.com>
2024-05-25 10:06:38 +0300

    ура я все сдала

---
+++ src/main/java/org/example/RBTree/NilNode.java
@@ -0,0 +1,10 @@
+package org.example.RBTree;
+
+class NilNode extends Node{
+    static final boolean BLACK = true;
+
+    public NilNode() {
+        super(0);
+        this.color = BLACK;
+    }
+}
---
+++ src/main/java/org/example/RBTree/Node.java
@@ -0,0 +1,13 @@
+package org.example.RBTree;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node parent;
+    boolean color;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
